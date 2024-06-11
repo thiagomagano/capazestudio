4a0b0e28345942fe58d2c3dacff56c6b54a86c05
Thiago Magano <thiagomagano1993@outlook.com>
2024-06-11 11:58:26 -0300

    wip: headline

--- src/lib/components/Headline.svelte
+++ src/lib/components/Headline.svelte
@@ -1,36 +1,17 @@
 <script>
 </script>
 
-<section
-  id="Headline"
-  class="bg-secondary text-black h-screen grid place-items-center"
->
-  <div class="flex gap-4 flex-col">
-    <h1 class="text-3xl font-bold">Capazes de Solucionar</h1>
-    <button class="text-secondary bg-primary p-4n">Fale com a Gente</button>
+<section id="Headline" class="bg-secondary text-black w-full grid-cols-3 grid">
+  <div class="grid place-items-center h-full">
+    <button class="text-secondary bg-primary p-4 text-2xl"
+      >Fale com a Gente</button
+    >
   </div>
-  <p class="font-long">
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error eum
-    architecto asperiores ut omnis? Nulla maiores veniam, voluptatum magnam
-    perspiciatis reiciendis inventore amet cumque dolorum repellat voluptas nam
-    id excepturi. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Error eum architecto asperiores ut omnis? Nulla maiores veniam, voluptatum
-    magnam perspiciatis reiciendis inventore amet cumque dolorum repellat
-    voluptas nam id excepturi. Lorem ipsum dolor sit amet consectetur,
-    adipisicing elit. Error eum architecto asperiores ut omnis? Nulla maiores
-    veniam, voluptatum magnam perspiciatis reiciendis inventore amet cumque
-    dolorum repellat voluptas nam id excepturi.
-  </p>
-  <p class="font-display">
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error eum
-    architecto asperiores ut omnis? Nulla maiores veniam, voluptatum magnam
-    perspiciatis reiciendis inventore amet cumque dolorum repellat voluptas nam
-    id excepturi. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Error eum architecto asperiores ut omnis? Nulla maiores veniam, voluptatum
-    magnam perspiciatis reiciendis inventore amet cumque dolorum repellat
-    voluptas nam id excepturi. Lorem ipsum dolor sit amet consectetur,
-    adipisicing elit. Error eum architecto asperiores ut omnis? Nulla maiores
-    veniam, voluptatum magnam perspiciatis reiciendis inventore amet cumque
-    dolorum repellat voluptas nam id excepturi.
-  </p>
+  <div>
+    <div class="text-[12rem] font-bold uppercase grid grid-rows-3">
+      <h1 class="text-left">Capazes</h1>
+      <h1 class="text-right">de</h1>
+      <h1 class="text-left">Solucionar</h1>
+    </div>
+  </div>
 </section>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,11 +1,11 @@
 <nav
-  class="bg-secondary text-black h-auto p-4 flex flex-row justify-between w-full"
+  class="bg-secondary text-black py-12 px-14 flex flex-row justify-between w-full items-center h-[70px]"
 >
   <div>
-    <img src="" alt="Logo Capaz Studio" />
+    <img src="/svg/Logo-nav-bar.svg" alt="Logo Capaz Studio" class="w-48" />
   </div>
 
-  <ul class="flex justify-between gap-4">
+  <ul class="flex gap-6 uppercase items-center text-xl font-medium">
     <li>Serviços</li>
     <li>Quem Somos</li>
     <li>Time</li>
