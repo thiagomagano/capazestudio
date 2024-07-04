0df7cb0e151a9ed0326bc20b1d1e9f39e71e2702
Thiago Magano <thiagomagano1993@outlook.com>
2024-07-04 12:10:48 -0300

    Implement hamburguer menu

--- src/lib/components/sections/Navbar.svelte
+++ src/lib/components/sections/Navbar.svelte
@@ -1,10 +1,14 @@
 <script>
   import scrollIntoView from "../../utils/scrollIntoView";
+  import { Hamburger } from "svelte-hamburgers";
+  import Menu from "$lib/components/ui/hamburguer/MenuHamburguer.svelte";
+
+  let open;
 </script>
 
 <nav class="bg-secondary text-black w-full border-gray-500">
   <div
-    class="flex flex-row justify-between items-center px-12 py-8 3xl:container 3xl:mx-auto 3xl:px-0"
+    class="flex flex-row justify-between items-center px-3 lg:px-12 py-8 3xl:container 3xl:mx-auto 3xl:px-0"
   >
     <div class="w-48">
       <img src="/svg/Logo-nav-bar.svg" alt="Logo Capaz Studio" class="w-full" />
@@ -27,11 +31,15 @@
         <a href="#contato" on:click|preventDefault={scrollIntoView}> Contato</a>
       </li>
     </ul>
+    <div class="block md:hidden">
+      <Hamburger bind:open --color="black" />
+    </div>
   </div>
+  <Menu bind:open />
 </nav>
 
 <style lang="postcss">
   a {
-    @apply font-medium transition-all duration-75 ease-in-out hover:text-capaz-orange-500;
+    @apply font-medium transition-all duration-75 ease-linear hover:text-capaz-orange-500;
   }
 </style>
---
+++ src/lib/components/ui/hamburguer/MenuHamburguer.svelte
@@ -0,0 +1,66 @@
+<script>
+  import { fly, scale } from "svelte/transition";
+  import { quadOut } from "svelte/easing";
+  import scrollIntoView from "$lib/utils/scrollIntoView";
+
+  export let open;
+
+  const links = [
+    {
+      link: "servicos",
+      label: "Serviços",
+    },
+    {
+      link: "quemsomos",
+      label: "Quem Somos",
+    },
+    {
+      link: "time",
+      label: "Time",
+    },
+    {
+      link: "contato",
+      label: "Contato",
+    },
+  ];
+</script>
+
+{#if open}
+  <div
+    class="bar lg:hidden border-capaz-orange-400"
+    transition:scale={{ duration: 200, easing: quadOut, opacity: 1 }}
+  ></div>
+
+  <div class="menu flex flex-col gap-4 lg:hidden">
+    {#each links as { link, label }, i}
+      <a
+        href="#{link}"
+        transition:fly={{ y: -15, delay: 50 * i }}
+        class="uppercase inline-block"
+        on:click|preventDefault={scrollIntoView}
+      >
+        {label}
+      </a>
+    {/each}
+  </div>
+
+  <div
+    class="bar lg:hidden border-capaz-orange-400"
+    transition:scale={{ duration: 200, easing: quadOut, opacity: 1 }}
+  ></div>
+{/if}
+
+<style>
+  .menu {
+    text-align: center;
+    font-size: 1.2em;
+    padding: 1em;
+    color: #000000;
+  }
+
+  .bar {
+    border-style: solid;
+    border-width: 1px;
+    height: 0;
+  }
+</style>
