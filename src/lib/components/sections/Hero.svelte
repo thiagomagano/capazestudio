a7db315b09711e13ca45f79bf934f5a3770e43f9
Thiago Magano <thiagomagano1993@outlook.com>
2024-08-02 18:18:06 -0300

    wip: ajuste setas

--- src/lib/components/sections/Hero.svelte
+++ src/lib/components/sections/Hero.svelte
@@ -12,30 +12,32 @@
     class="container mx-auto grid grid-cols-3 place-items-end w-full lg:place-items-stretch lg:grid-cols-4"
   >
     <div
-      class="hidden lg:col-start-4 lg:row-start-1 text-gray-500 xl:text-2xl lg:text-xl xl:leading-none font-normal lg:flex flex-col text-right items-center justify-center"
+      class="relative hidden lg:col-start-4 lg:row-start-1 text-gray-500 xl:text-2xl lg:text-xl xl:leading-none font-medium lg:flex flex-col text-right items-center justify-center"
     >
       <div>
         <h2>Publicidade,</h2>
         <h2>Design &</h2>
         <h2>Tecnologia</h2>
       </div>
+      <div
+        class="hidden lg:absolute lg:block w-[200px] -right-[20%] -top-[5%] xl:w-[240px] xl:-right-[17%] xl:-top-[10%] 2xl:w-[265px] 2xl:-right-[10%] 2xl:-top-[5%]"
+      >
+        <img
+          src="/svg/Setas/setadireita.svg"
+          alt="Seta Apontando para botão"
+          class="w-full"
+          id="setaDireita"
+        />
+      </div>
     </div>
 
     <div
-      class="relative hidden col-start-4 row-start-2 text-gray-500 xl:text-2xl lg:text-xl xl:leading-none font-normal w-full lg:flex items-center justify-center text-right"
+      class="relative hidden col-start-4 row-start-2 text-gray-500 xl:text-2xl lg:text-xl xl:leading-none font-medium w-full lg:flex items-center justify-center text-right"
     >
       <div>
         <h2>Criatividade</h2>
         <h2>com</h2>
         <h2>Propósito</h2>
-      </div>
-      <div class="hidden lg:absolute -right-20 xl:top-[50%] w-[50px]">
-        <img
-          src="/svg/Setas/PC_1.svg"
-          alt="Seta Apontando para botão"
-          class="w-full"
-          id="setaEsquerda"
-        />
       </div>
 
       <div
@@ -52,7 +54,7 @@
       <a
         href="#contato"
         on:click|preventDefault={scrollIntoView}
-        class="py-5 bg-primary border-primary border-2 text-secondary text-3xl w-[60%] text-center flex justify-center items-center gap-2"
+        class="py-4 bg-primary text-secondary text-3xl lg:w-[60%] text-center"
       >
         E Você, quer ser capaz?
       </a>
@@ -60,9 +62,11 @@
 
     <h1 class="relative row-start-1 col-start-1 col-span-3 place-self-start">
       Capazes
-      <div class="hidden size-[160px] lg:absolute top-1/2 -left-40">
+      <div
+        class="hidden lg:absolute lg:block lg:w-[230px] lg:-top-6 lg:-left-[22%] xl:w-auto xl:-top-16 xl:-left-52 2xl:-top-6 2xl:-left-52"
+      >
         <img
-          src="/svg/Setas/PC.svg"
+          src="/svg/Setas/setaesquerda.svg"
           alt="Seta Apontando para Solucionar"
           class="w-full self-center"
           id="setaDireita"
@@ -70,10 +74,12 @@
       </div>
     </h1>
     <h1 class="row-start-2 col-start-1 relative place-self-start">de</h1>
-    <div class="row-start-3 col-span-4 relative 2xl:min-h-[16.5rem]">
+    <div
+      class="row-start-3 col-span-4 relative min-h-[4rem] sm:min-h-[7rem] md:min-h-[9rem] lg:min-h-[11.5rem] xl:min-h-[13.5rem] 2xl:min-h-[16.5rem] place-self-start"
+    >
       <TypeWriter
         texts={["Solucionar", "Humanizar", "Cativar", "Conectar", "Engajar"]}
-        cl="text-[3.90rem] font-medium uppercase leading-[1] tracking-normal sm:text-[7rem] md:text-[8.5rem] lg:text-[9rem] xl:text-[10rem] 2xl:text-[15.5rem]"
+        cl="text-right text-[3.8rem] font-medium uppercase leading-[0.9] tracking-normal sm:text-[7rem] md:text-[9rem] lg:text-[11.5rem] xl:text-[13.5rem] 2xl:text-[15.5rem]"
       />
     </div>
 
@@ -100,24 +106,22 @@
     </div>
   </div>
 
-  <div class="flex justify-center items-center pt-4">
-    <a
+  <a
+    href="#servicos"
+    on:click|preventDefault={scrollIntoView}
+    class="flex justify-center items-center mt-8"
+  >
+    <img
+      src="/svg/Seta-head.svg"
+      alt="Seta apontando para baixo"
+      class="w-[18px] md:w-[25px]"
       href="#servicos"
-      on:click|preventDefault={scrollIntoView}
-      class="flex justify-center items-center"
-    >
-      <img
-        src="/svg/Seta-head.svg"
-        alt="Seta apontando para baixo"
-        class="w-[25px]"
-        href="#servicos"
-      />
-    </a>
-  </div>
+    />
+  </a>
 </section>
 
 <style lang="postcss">
   h1 {
-    @apply text-right text-[3.90rem] font-medium uppercase leading-[0.9] tracking-normal sm:text-[7rem] md:text-[8.5rem] lg:text-[9rem] xl:text-[10rem] 2xl:text-[15.5rem];
+    @apply text-right text-[3.8rem] font-medium uppercase leading-[0.9] tracking-normal sm:text-[7rem] md:text-[9rem] lg:text-[11.5rem] xl:text-[13.5rem] 2xl:text-[15.5rem];
   }
 </style>
